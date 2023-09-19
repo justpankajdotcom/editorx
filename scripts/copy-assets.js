ec62108bc94e3431ee30c89f0149fa8a5bc9e9c5
Myriad-Dreamin <camiyoru@gmail.com>
2023-09-19 22:27:08 +0800

    get artifact with browser compiler

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -2,3 +2,4 @@
 const { copyFileSync } = require('fs');
 
 copyFileSync('node_modules/@myriaddreamin/typst-ts-renderer/pkg/typst_ts_renderer_bg.wasm', 'public/typst-renderer.wasm');
+copyFileSync('node_modules/@myriaddreamin/typst-ts-web-compiler/pkg/typst_ts_web_compiler_bg.wasm', 'public/typst-compiler.wasm');
